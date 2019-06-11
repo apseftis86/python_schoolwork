d02d7086c6ca34f8b807924bd7babcc7f0ee1216
Amanda Pseftis <amanda.pseftis@icloud.com>
2019-06-11 13:14:42 -0400

    adding files

---
+++ flask/flask_fundamentals/hello_flask/hello.py
@@ -0,0 +1,14 @@
+from flask import Flask  # Import Flask to allow us to create our app
+app = Flask(__name__)    # Create a new instance of the Flask class called "app"
+@app.route('/')          # The "@" decorator associates this route with the function immediately following
+def hello_world():
+    return 'Hello World!'  # Return the string 'Hello World!' as a response
+@app.route('/<name>')
+def hello_person(name):
+    return f'<h1>Hello {name.capitalize()}</h1>'
+@app.route('/users/<username>/<id>')
+def show_user_profile(username, id):
+    return "username: " + username + ", id: " + id
+
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
+    app.run(debug=True)    # Run the app in debug mode.
