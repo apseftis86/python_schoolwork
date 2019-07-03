de375b933a798f7c66b8bbc0a89d24c5aa867a75
amandapseftis <amanda.pseftis@jacobs.com>
2019-07-03 09:59:17 -0400

    trying to lessen the sql injection vulnerability on some of my applications

--- flask/flask_mysql/users/mysqlconnection.py
+++ flask/flask_mysql/users/mysqlconnection.py
@@ -6,8 +6,8 @@ import pymysql.cursors
 class MySQLConnection:
     def __init__(self, db):
         connection = pymysql.connect(host='localhost',
-                                     user='root',  # change the user and password as needed
-                                     password='',
+                                     user='my_user',  # change the user and password as needed
+                                     password='root',
                                      db=db,
                                      charset='utf8mb4',
                                      cursorclass=pymysql.cursors.DictCursor,
